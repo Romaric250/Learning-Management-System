5300fda0b4574864a0d1bcdd2d0a91d47bbee962
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-13 10:06:40 +0000

    feat some more updates done on the api routes

--- lms/app/api/user/route.ts
+++ lms/app/api/user/route.ts
@@ -11,6 +11,12 @@ export async function GET(req:Request){
         const getusers = await db.user.findMany()
 
 
+    if(!getusers){
+        return new NextResponse("No user found", {status:404})
+    }
+
+    return new NextResponse(JSON.stringify(getusers), {status:200})
+
 
         
     } catch (error:any) {
