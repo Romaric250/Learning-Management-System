71fe877a3c4f9f40a37eacc5c62cb3d739844870
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-13 10:11:52 +0000

    feat some updates done here, and sme stuff done later

--- lms/app/api/user/route.ts
+++ lms/app/api/user/route.ts
@@ -25,3 +25,29 @@ export async function GET(req:Request){
         
     }
 }
+
+
+export async function POST(req:Request){
+    try {
+        const {name, email, password} = await req.json()
+
+        if(!name || !email || !password){
+            return new NextResponse("Please fill in all fields", {status:400})
+        }
+
+        const createUser = await db.user.create({
+            data:{
+                name,
+                email
+                
+            }
+        })
+
+        return new NextResponse(JSON.stringify(createUser), {status:201})
+        
+    } catch (error:any) {
+        console.log(error.message)
+        return new NextResponse("An internal server error occured", {status:500})
+        
+    }
+}
