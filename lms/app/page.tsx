3ed26fbc46119d0cea1eb6fc3be9b867145f31c6
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-13 06:07:46 +0000

    feat some more updates done on the readme

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos eos quibusdam sit non expedita vero praesentium veritatis dolor blanditiis omnis!
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos eos quibusdam sitand an i don know what the fuc is wrong here expedita vero praesentium veritatis dolor blanditiis omnis!
     </main>
   );
 }
